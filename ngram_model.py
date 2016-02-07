582fb9c764b39787d8081da4c87ff472b89c7515
Sarah Wooders <sarahwooders@gmail.com>
2016-02-06 23:22:27 -0500

    added some tree stuf

--- ngram_model.py
+++ ngram_model.py
@@ -1,5 +1,7 @@
 import cPickle, heapq, Queue
 from collections import Counter
+
+k = 3
          
 def get_token_counts(lines):
     all_tokens = Counter()
@@ -11,25 +13,57 @@ def get_token_counts(lines):
     return all_tokens
 
 class NGramNode:
-    def __init__(self):
-        self.edges = {}
 
-    def add_edge(self, edge):
-        if not edge in self.edges:
-            self.edges[edge] = NGramNode()
+    def __init__(self, value):
+        self.successor = {}
+        self.value = value
+        self.count = 1
+        self.best_succ = None #the child that is most probable, and how many times it occurs
+        self.best_count = 0
 
-    def add_num(self, edge):
-        pass
+    def add_successor(self, token):
+        if not token in self.successor:
+            self.successor[token] = NGramNode(token)
+        else:
+            self.successor[token].count += 1
+
+        #Update most likely successor 
+        if self.best_succ == null || self.successor[token].count > self.best_count:
+            self.best_succ = token
+            self.best_count = self.successor[token].count
+
+        return self.successor[token]
 
 class NGramModel:
-    def __init__(self, corpus_sentences):
+    def __init__(self, corpus_sentences, root):
         for i in corpus_sentences:
-            self.process_sentence(sentence)
+            self.process_sentence(sentence, root)
 
-        self.model = 
+    def process_sentence(self, sentence, root):
+        sentence = sentence.split()
+        index = 0;
+        for index in range(len(sentence))
+            node = root.add_successor(sentence[index])
+            tokens = sentence[i:i+k]
+            last_node = node
+            for token in tokens:
+                n = last_node.add_successor(token)
+                last_node = n
 
-    def process_sentence(self, sentence):
-        pass
+    def process_input(input, root):
+        input = input.split()
+        if len(input) > k:
+            tokens = input[len(input) - k:len(input)]    
+        else:
+            tokens = input[i:i+len(input)]   
+        n = root.successor[tokens[0]]
+        for t in tokens:
+
+
+
+
+
+
 
 if __name__ == "__main__":
     _, corpus_path = sys.argv
@@ -38,6 +72,8 @@ if __name__ == "__main__":
 
     counts = get_token_counts(lines)
     K = 15 # number of counts before
+
+    #DECLARE ROOT HERE 
 
     def is_wildcard(token):
         return counts[token.strip()] < K + 1
